d5246ac3453d8a56da0cafcb6ea13c292317f12c
caue <caueweber22@gmail.com>
2025-01-14 00:25:33 -0300

    Views tests

--- foods/tests.py
+++ foods/tests.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+from foods import views
 
 class FoodsUrlsTests(TestCase):
-    def test_main_url(self):
+    def test_food_main_url(self):
         main_url= reverse('foods:main')
         self.assertEqual(main_url, '/')
 
@@ -13,3 +14,17 @@ class FoodsUrlsTests(TestCase):
     def test_add_food_url(self):
         add_food_url= reverse('foods:add_food')
         self.assertEqual(add_food_url, '/add/')
+
+
+class FoodsViewsTests (TestCase):
+    def test_food_main_view_is_correct(self):
+        view_main = resolve(reverse('foods:main'))
+        self.assertIs(view_main.func, views.main)
+
+    def test_show_food_view_is_correct(self):
+        show_food_view = resolve(reverse('foods:show_food'))
+        self.assertIs(show_food_view.func, views.show_food)
+
+    def test_add_food_view_is_correct(self):
+        add_food_view = resolve(reverse('foods:add_food'))
+        self.assertIs(add_food_view.func, views.add_food)
--- users/tests.py
+++ users/tests.py
@@ -1,11 +1,22 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+from users import views
 
-class FoodsUrlsTests(TestCase):
+class UsersUrlsTests(TestCase):
     def test_user_register_url(self):
         register_url= reverse('users:register')
-        self.assertEqual(register_url, 'register/')
+        self.assertEqual(register_url, '/user/register/')
+
+    def test_user_login_url(self):
+        login_url= reverse('users:login')
+        self.assertEqual(login_url, '/user/login/')
 
 
+class UsersViewsTests(TestCase):
+    def test_user_login_view_is_correct(self):
+        user_login_view = resolve(reverse('users:login'))
+        self.assertIs(user_login_view.func, views.login)
 
-    
+    def test_user_register_view_is_correct(self):
+        user_register_view = resolve(reverse('users:register'))
+        self.assertIs(user_register_view.func, views.register)
