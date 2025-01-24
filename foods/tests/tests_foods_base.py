10098e16dba28a887ab6cdd9037ca866631009c8
caue <caueweber22@gmail.com>
2025-01-23 21:49:53 -0300

    More foods_model tests

--- foods/tests/tests_foods_base.py
+++ foods/tests/tests_foods_base.py
@@ -1,5 +1,4 @@
 from django.test import TestCase
-from django.urls import reverse, resolve
 from foods import views
 from foods.models import Food, Category 
 from django.contrib.auth.models import User
--- foods/tests/tests_foods_models.py
+++ foods/tests/tests_foods_models.py
@@ -1,6 +1,3 @@
-from django.test import TestCase
-from django.urls import reverse, resolve
-from foods import views
 from foods.models import Food
 from .tests_foods_base import FoodsTestBases
 from django.core.exceptions import ValidationError
@@ -28,6 +25,27 @@ class FoodsModelsTests (FoodsTestBases):
       with self.assertRaises(ValidationError):
         self.food.full_clean()        
 
+   #Test food price max length
+   def test_food_price_max_length(self):
+      self.food.price = 12345678901234
+      
+      with self.assertRaises(ValidationError):
+         self.food.full_clean()
+
+   #Test food price max decimal places
+   def test_food_price_max_decimal_places(self):
+      self.food.price = 123.2324
+      
+      with self.assertRaises(ValidationError):
+         self.food.full_clean()      
+
+   #Test category category_type max length 
+   def test_category_category_type_max_length(self):
+      self.category_test.category_type = 'a' * 60
+      
+      with self.assertRaises(ValidationError):
+         self.category_test.full_clean()      
+
    #Test if foods are set by default to true at creation
    def test_food_is_available_is_default_true(self):
       food = Food.objects.create(
@@ -39,6 +57,37 @@ class FoodsModelsTests (FoodsTestBases):
       
       self.assertEqual(food.is_available, True)
 
+   #Test foods images are set by default to "no_image"
+   def test_food_image_is_default_text_is_ok(self):
+      food = self.make_food()
+      
+      self.assertEqual(food.image, "no_image")
 
+   #Test if food name cant be null
+   def test_if_food_name_cant_be_null(self):
+      self.food.name = None
+      
+      with self.assertRaises(ValidationError):
+         self.food.full_clean()  
+  
+   #Test food price cant be null
+   def test_if_food_price_cant_be_null(self):
+      self.food.price = None
+      
+      with self.assertRaises(ValidationError):
+         self.food.full_clean()
 
+   #Test food category cant be null
+   def test_if_food_category_cant_be_null(self):
+      self.food.category = None
+      
+      with self.assertRaises(ValidationError):
+         self.food.full_clean()        
+
+   #Test if category category_type can be null
+   def test_if_category_category_type_cant_be_null(self):
+      self.category_test.category_type = None
+      
+      with self.assertRaises(ValidationError):
+         self.category_test.full_clean()    
 
